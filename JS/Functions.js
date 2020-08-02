7a52450d7bc44308077674e99ecf61a322cd03e7
Mohammad Hashem <67805654+MHH2020@users.noreply.github.com>
2020-08-02 14:29:56 +0300

    onload event To load Home page

--- JS/Functions.js
+++ JS/Functions.js
@@ -10,37 +10,37 @@
 // btn_Women.addEventListener("click", link1);
 // btn_Baby.addEventListener("click", link1);
 
-
-
 // function link1() {
 
 //     frm_Show.src = "https://www.google.com/";
 
-
 // }
 
-   //  alert("Pages/Home.html");var 
-    // alert(item.getAttribute('id'));
-   // var x = document.getElementById("frm_Show").src;
-    // document.getElementById("demo").innerHTML = x;
+//  alert("Pages/Home.html");var
+// alert(item.getAttribute('id'));
+// var x = document.getElementById("frm_Show").src;
+// document.getElementById("demo").innerHTML = x;
 
 function OpenPage(item) {
- 
-    if (item.getAttribute('id') === "btn_Home") {
-         document.getElementById("frm_Show").src = "Pages/Home.html";   
-    }
-    if (item.getAttribute('id') === "btn_Men") {
-        document.getElementById("frm_Show").src = "Pages/Men_Shose.html";
-    }
-    if (item.getAttribute('id') === "btn_MenSport") {
-        document.getElementById("frm_Show").src = "Pages/Men_Sport_Shose.html";
-    }
-    if (item.getAttribute('id') === "btn_Women") {
-        document.getElementById("frm_Show").src = "Pages/Women_Shose.html";
-    }
-    if (item.getAttribute('id') === "btn_Baby") {
-        document.getElementById("frm_Show").src = "Pages/Baby_Shose.html";
-    }
+  if (item.getAttribute("id") === "btn_Home") {
+    document.getElementById("frm_Show").src = "Pages/Home.html";
+  }
+  if (item.getAttribute("id") === "btn_Men") {
+    document.getElementById("frm_Show").src = "Pages/Men_Shose.html";
+  }
+  if (item.getAttribute("id") === "btn_MenSport") {
+    document.getElementById("frm_Show").src = "Pages/Men_Sport_Shose.html";
+  }
+  if (item.getAttribute("id") === "btn_Women") {
+    document.getElementById("frm_Show").src = "Pages/Women_Shose.html";
+  }
+  if (item.getAttribute("id") === "btn_Baby") {
+    document.getElementById("frm_Show").src = "Pages/Baby_Shose.html";
+  }
 }
 
-
+function loadFunction(item) {
+  if (item.getAttribute("id") === "frm_Show") {
+    doc.getElementById("frm_Show").src = "Pages/Home.html";
+  }
+}
